84b1815279abf89266b18abc2edfe80f974591a9
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-11 15:45:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{:key1 => "Key 1"}
+	{:railroads => "Key 1"}
 end
 
 def monopoly_with_second_tier
